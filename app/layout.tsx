2863324b2aa5b935578c6130a6be6acba75f5ea5
kautilyadevaraj <kautilyadk@gmail.com>
2025-07-08 23:27:00 +0530

    enabled vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
 
 const figtree = Figtree({
   subsets: ["latin"],
@@ -35,6 +36,7 @@ export default function RootLayout({
         <main className="pt-16">{children}</main>
         <Toaster richColors closeButton />
         </ThemeProvider>
+        <Analytics/>
       </body>
     </html>
   );
