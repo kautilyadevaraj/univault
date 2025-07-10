00eabd2ac6e758f3071f937bf268a621edde4690
kautilyadevaraj <kautilyadk@gmail.com>
2025-07-10 17:36:50 +0530

    removed unwanted prisma client configs

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,9 @@
 // lib/prisma.ts
 import { PrismaClient } from "./generated/prisma";
+import { withOptimize } from "@prisma/extension-optimize";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = new PrismaClient().$extends(
+  withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY! })
+);
 
 export const db = prisma;
