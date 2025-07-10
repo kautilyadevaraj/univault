ad4e623b4cb11e71def13e251e9d032216d04494
kautilyadevaraj <kautilyadk@gmail.com>
2025-07-10 17:03:20 +0530

    added prisma optimize extension to analyze queries

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,17 @@
-// lib/prisma.ts
 import { PrismaClient } from "./generated/prisma";
+import { withOptimize } from "@prisma/extension-optimize";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+function createExtendedPrismaClient() {
+  return new PrismaClient().$extends(
+    withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY! })
+  );
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const globalForPrisma = global as unknown as {
+  prisma?: ReturnType<typeof createExtendedPrismaClient>;
+};
+
+export const prisma = globalForPrisma.prisma ?? createExtendedPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
