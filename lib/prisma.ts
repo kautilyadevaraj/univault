baefafe52af293f338fd557c74a542ca84ccca86
kautilyadevaraj <kautilyadk@gmail.com>
2025-07-10 17:59:12 +0530

    added singleton client instance

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,8 +2,17 @@
 import { PrismaClient } from "./generated/prisma";
 import { withOptimize } from "@prisma/extension-optimize";
 
-const prisma = new PrismaClient().$extends(
-  withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY! })
-);
+const createPrismaClient = () =>
+  new PrismaClient().$extends(
+    withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY! })
+  );
+
+const globalForPrisma = global as unknown as {
+  prisma?: ReturnType<typeof createPrismaClient>;
+};
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export const db = prisma;
