1ec7c524375f433aed7f4f682aaf01ed59711119
kautilyadevaraj <kautilyadk@gmail.com>
2025-07-10 17:25:39 +0530

    Revert "added prisma optimize extension to analyze queries"
    This reverts commit ad4e623b4cb11e71def13e251e9d032216d04494.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,9 @@
+// lib/prisma.ts
 import { PrismaClient } from "./generated/prisma";
-import { withOptimize } from "@prisma/extension-optimize";
 
-function createExtendedPrismaClient() {
-  return new PrismaClient().$extends(
-    withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY! })
-  );
-}
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-const globalForPrisma = global as unknown as {
-  prisma?: ReturnType<typeof createExtendedPrismaClient>;
-};
-
-export const prisma = globalForPrisma.prisma ?? createExtendedPrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
